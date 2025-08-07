963babfb098f83e68cde3261f1a835570ca1e0f7
PengZack <a0922820897@gmail.com>
2025-08-07 16:15:57 +0800

    初始化

---
+++ script.js
@@ -0,0 +1,125 @@
+
+let food = parseInt(prompt("今天吃多少????"))
+let percent = parseInt(prompt("小費幾趴??")) / 100
+let tipamount = food * percent
+let total = food + tipamount
+console.log("小費金額"+tipamount)
+console.log("總金額"+total)
+/*
+<script>
+  function showSubMenu() {
+    document.getElementById('mainMenu').style.display = 'none';
+    document.getElementById('aboutMenu').style.display = 'block';
+  }
+  function backToMainMenu() {
+    document.getElementById('aboutMenu').style.display = 'none';
+    document.getElementById('mainMenu').style.display = 'block';
+  }
+
+  function showServiceMenu() {
+  document.getElementById('mainMenu').style.display = 'none';
+  document.getElementById('serviceMenu').style.display = 'block';
+  }
+  function backToServiceMenu() {
+    document.getElementById('serviceMenu').style.display = 'none';
+    document.getElementById('mainMenu').style.display = 'block';
+  }
+
+  function showTrainingMenu() {
+  document.getElementById('mainMenu').style.display = 'none';
+  document.getElementById('trainingMenu').style.display = 'block';
+  }
+  function backToTrainingMenu() {
+    document.getElementById('trainingMenu').style.display = 'none';
+    document.getElementById('mainMenu').style.display = 'block';
+  }
+
+  function showEventmsgMenu() {
+    document.getElementById('mainMenu').style.display = 'none';
+    document.getElementById('eventmsgMenu').style.display = 'block';
+  }
+  function backToEventmsgMenu() {
+    document.getElementById('eventmsgMenu').style.display = 'none';
+    document.getElementById('mainMenu').style.display = 'block';
+  }
+
+  function showNewsMenu() {
+    document.getElementById('mainMenu').style.display = 'none';
+    document.getElementById('newsMenu').style.display = 'block';
+  }
+  function backToNewsMenu() {
+    document.getElementById('newsMenu').style.display = 'none';
+    document.getElementById('mainMenu').style.display = 'block';
+  }
+
+</script>
+-->*/
+/*<!--  
+    <-- 主選單 
+    <div id="mainMenu">
+      <div class="menu-item">
+        <a href="#" onclick="showSubMenu()">關於我們</a>
+      </div>
+      <div class="menu-item">
+        <a href="#" onclick="showServiceMenu()">服務項目</a>
+      </div>
+      <div class="menu-item">
+        <a href="#" onclick="showTrainingMenu()">教育訓練</a>
+      </div>
+      <div class="menu-item">
+        <a href="#" onclick="showEventmsgMenu()">活動訊息</a>
+      </div>
+      <div class="menu-item">
+        <a href="#" onclick="showNewsMenu()">新知分享</a>
+      </div>
+    </div>
+
+    <-- 關於我們：子選單 
+    <div id="aboutMenu"  class="allmenu"  style="display: none;">
+      <div class="submenu-header">
+        <a href="#" onclick="backToMainMenu()">← 關於我們</a>
+      </div>
+      <div class="menu-item"><a href="#">關於博創</a></div>
+      <div class="menu-item"><a href="#">企業文化</a></div>
+      <div class="menu-item"><a href="#">服務據點</a></div>
+      <div class="menu-item"><a href="#">企業夥伴</a></div>
+    </div>
+
+  <div id="serviceMenu" class="allmenu" style="display: none;">
+    <div class="submenu-header">
+      <a href="#" onclick="backToServiceMenu()">← 服務項目</a>
+    </div>
+    <div class="menu-item"><a href="#">個資保護顧問服務</a></div>
+    <div class="menu-item"><a href="#">資安防護顧問服務</a></div>
+    <div class="menu-item"><a href="#">資安檢測技術服務</a></div>
+  </div>
+
+  <div id="trainingMenu" class="allmenu" style="display: none;">
+  <div class="submenu-header">
+    <a href="#" onclick="backToTrainingMenu()">← 教育訓練</a>
+  </div>
+  <div class="menu-item"><a href="#">資訊安全管理領域</a></div>
+  <div class="menu-item"><a href="#">個人資料管理領域</a></div>
+  <div class="menu-item"><a href="#">企業管理領域</a></div>
+  <div class="menu-item"><a href="#">隱私權保護政策</a></div>
+  </div>
+
+  <div id="eventmsgMenu" class="allmenu" style="display: none;">
+  <div class="submenu-header">
+    <a href="#" onclick="backToEventmsgMenu()">← 活動訊息</a>
+  </div>
+  <div class="menu-item"><a href="#">課程訊息</a></div>
+  <div class="menu-item"><a href="#">個人資料管理領域</a></div>
+  </div>
+
+  <div id="newsMenu" class="allmenu" style="display: none;">
+  <div class="submenu-header">
+    <a href="#" onclick="backToNewsMenu()">← 新知分享</a>
+  </div>
+  <div class="menu-item"><a href="#">ISO國際認證</a></div>
+  <div class="menu-item"><a href="#">資訊安全新知</a></div>
+  <div class="menu-item"><a href="#">職場經營</a></div>
+  </div>
+
+</div>
+-->*/
